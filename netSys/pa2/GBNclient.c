67962d4e598e7f5a73a4499a6334206a599bf2d9
gabesotto <gabesotto@gmail.com>
2013-10-07 00:52:18 -0600

    finished client code ish

--- netSys/pa2/GBNclient.c
+++ netSys/pa2/GBNclient.c
@@ -66,6 +66,7 @@ int main(int argc, char *argv[]) {
 	file_size = ftell(fp);
 	rewind(fp);
 
+	//read file into window buffer
 	bzero(&window,sizeof(window));
 	size_t result = fread(window, 1, file_size, fp);
 	if(result != file_size){
@@ -73,40 +74,107 @@ int main(int argc, char *argv[]) {
 		fclose(fp);
 	}
 	fclose(fp);
-	int windowSize = 6; 
-	int pCounter = 0;
+	
+	//Setup select()
+	struct timeval tv;
+    fd_set readfds;
+    
+    //set timeout
+    tv.tv_sec = 0;
+    tv.tv_usec = 50000;
+    FD_ZERO(&readfds);
+    FD_SET(sd, &readfds);
+	
+	int nbytes = -1; //number of bytes sent
+	int sws = 6; //Send Window Size
+	int lar = -1; //last ack received 
+	int i = 0;
+	//TODO: CALCULATE LAST FRAME! PUT SPECIAL FLAG ON LAST FRAME!
 	//While the EOF has not been reached.
-	while(true){
+	while(1){
 		//Send window
-		int i = 0;
-		for(i = 0; i < windowSize; i++){
-			//Wrap header, send packet. 
-		}
-		//Wait for acks, move window pointer as neccesary
+		for(i=lar+1; i <= lar+sws; i++){
+			/* Prepare packet */
+			char seqNum[4];  //header
+			char tempSeqNum[4];
+			char payload[PAYLOADSIZE]; //payload to be sent
+			
+			//Prepare Header (Padded with 0's to make 3 digits ie(001, 020))
+			sprintf(tempSeqNum, "%d", i); //copy int into buffer
+			if(i < 10){//pad with 2 zeros
+				strncpy(seqNum, "00", 4);
+			}else if(i < 100){//pad with 1 zero
+				strncpy(seqNum, "0", 4);
+			}
+			strcat(seqNum, tempSeqNum);
+			//printf("header after prep: %s\n", seqNum);
+			
+			//prepare payload
+			int beginIndex = i*1024;
+			bzero(&payload, PAYLOADSIZE);
+			memcpy(payload, window + beginIndex, 1024);
+			int headerSize = sizeof(seqNum);        
+			int packetSize = PAYLOADSIZE + headerSize;
+			
+			//prepare packet
+			char packet[packetSize];
+			bzero(&packet, packetSize);
+			//TODO:Change to memcpy when writing binary file?
+			//strncat(packet, seqNum, headerSize); //copy header
+			//strncat(packet, payload ,PAYLOADSIZE); //copy payload
+			memcpy(&packet[0], seqNum, headerSize);
+			memcpy(&packet[4], payload, PAYLOADSIZE); 
+			
+			//printf("Size of packet: %d\n", packetSize);
+			//printf("packet: %s\n", &packet[0]);
+			printf("Sending packet %d\n", i);
+			nbytes = sendto_(sd, packet, packetSize, 0, (struct sockaddr *) &remoteServAddr, sizeof(remoteServAddr));
+		}//end for
 		
+		//TODO: Maybe put a while loop here
+		//wait for and process acks
+		char ack[4];
+		if(select(sd+1, &readfds, NULL, NULL, &tv)<0){
+			perror("Select error");
+		}
+		if (FD_ISSET(sd, &readfds)){
+			//ACK Received process ack
+			nbytes = recvfrom(sd, ack, sizeof(ack), 0, (struct sockaddr*)&remoteServAddr, &remoteServAddrLen);
+			printf("Ack %s received!\n", ack);
+			int ackNum = atoi(ack);
+			if(ackNum == (lar + 1)){
+				lar += 1;
+			}
+			
+		}else{
+			//Timeout Resend window
+			printf("Timed out. Resend window!\n");
+			continue;
+		}
+	}//end while
+}//end main
+		/*
+		//Wait for acks, move window pointer as neccesary
+		if(select(sd+1, &readfds, NULL, NULL, &tv)<0){
+			perror("Select error");
+		}
+    
+		//ACK Received process ack
+		if (FD_ISSET(sd, &readfds)){
+			printf("Ack received!\n");
+			nbytes = recvfrom(sd, ack, sizeof(ack), 0, (struct sockaddr*)&remoteServAddr, &remoteServAddrLen);
+			printf("ack: %s\n", ack);
+		
+		//Timeout Resend window
+		}else{	
+			printf("Timed out.\n");
+			continue;
+		}
 		
 	}
 	
 	
-	/* Send packet */
-	int nbytes;
-	char seqNum[4];
-	char payload[PAYLOADSIZE];
-	strcpy(seqNum, "001");
-	strcpy(payload, "To be or not to be");
-	int headerSize = sizeof(seqNum);
-	int packetSize = PAYLOADSIZE + headerSize;
-	char packet[packetSize];
-	
-	bzero(&packet, packetSize);
-	//Change to memcpy when writing binary file
-	strncat(packet, seqNum, headerSize); //copy header
-	strncat(packet, payload ,PAYLOADSIZE); //copy payload
-	
-	//printf("Size of packet: %d\n", packetSize);
-	//printf("packet: %s\n", packet);
-	nbytes = sendto_(sd, packet, packetSize, 0, (struct sockaddr *) &remoteServAddr, sizeof(remoteServAddr));
-	
+
 	//Wait for ack
 	struct timeval tv;
     fd_set readfds;
@@ -133,7 +201,7 @@ int main(int argc, char *argv[]) {
 		//Timeout Resend window
         printf("Timed out.\n");
 	}
-}
+}*/
 
 
 
--- netSys/pa2/GBNserver.c
+++ netSys/pa2/GBNserver.c
@@ -47,31 +47,50 @@ int main(int argc, char *argv[]) {
 		exit(1); 
 	}
 
-	/* Receive message from client */
 	struct sockaddr_in cliAddr;
 	unsigned int cliLen;
 	int nbytes;
 	char recvmsg[1028];
-	bzero(recvmsg,sizeof(recvmsg));
-	cliLen = sizeof(cliAddr);
-	nbytes = recvfrom(sd, &recvmsg, sizeof (recvmsg), 0, (struct sockaddr *) &cliAddr, &cliLen);
-	
-	printf("client says %s \n", recvmsg);
-	
-	char ack[4];
+	char packetNum[4];
 	char payload[1024];
-	stripHeader(recvmsg, ack);
-	memmove(recvmsg, recvmsg+3, sizeof(recvmsg));
-	memcpy(payload, recvmsg, sizeof(payload));
-	//printf("header stripped: %s \n", ack);
-	//printf("payload stripped; %s \n", payload);
 	
-	/* Send Ack */
-	nbytes = sendto_(sd, ack, sizeof(ack),0, (struct sockaddr *) &cliAddr, cliLen);
-	printf("nbytes sent: %d\n", nbytes);
+	while(1){
+		
+		/* Receive message from client */
+		bzero(recvmsg,sizeof(recvmsg));
+		bzero(packetNum, sizeof(packetNum));
+		bzero(payload, sizeof(payload));
+		cliLen = sizeof(cliAddr);
+		nbytes = recvfrom(sd, &recvmsg, sizeof (recvmsg), 0, (struct sockaddr *) &cliAddr, &cliLen);
+		//printf("received %d bytes\n", nbytes);
+		//printf("client says %s \n", recvmsg);
+		
+		stripHeader(recvmsg, packetNum);
+		memmove(recvmsg, &recvmsg[3], sizeof(recvmsg));
+		memcpy(payload, recvmsg, sizeof(payload));
+		//printf("header stripped: %s \n", ack);
+		//printf("payload stripped; %s \n", payload);
+		int frameNum = atoi(packetNum);
+		printf("received packet #: %d\n", frameNum);
+		
+		if(){ //Packet is within receive window size (rws)
+			//write to payload to file buffer
+			//send ack back
+		}else if(){ //final packet flag
+			break;
+		}else{ //throw away packet
+			continue;
+		}
+
+		/* Send Ack */
+		nbytes = sendto_(sd, packetNum, sizeof(packetNum),0, (struct sockaddr *) &cliAddr, cliLen);
+	}
+	
+	close(sd);
+	return 0;
 }
 
-//Remove header from packet, returns int version of header
+//Remove header from packet
 void stripHeader(char packet[], char head[]){
 	int i = 0;
 	for(i = 0; i < 3; i++){
